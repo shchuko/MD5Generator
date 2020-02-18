6aac5e1f8365be0b169c6d7f4f82a2f62dbd9889
Vladislav Yaroshchuk <yaroshchuk2000@gmail.com>
2020-02-18 19:42:41 +0300

    MD5GenUtilsUnitTests add file hash test

--- src/test/java/shchuko/md5genlib/MD5GenUtilsUnitTests.java
+++ src/test/java/shchuko/md5genlib/MD5GenUtilsUnitTests.java
@@ -27,4 +27,26 @@ public class MD5GenUtilsUnitTests {
         String actual = MD5GenUtils.getStringHash(s).getHashHexString(false);
         assertEquals(expected, actual);
     }
+
+    @Test
+    public void getHashOfFileTest() {
+        ClassLoader classLoader = getClass().getClassLoader();
+
+        URL resource = classLoader.getResource("fileHashTest1.mp3");
+        if (resource == null) {
+            fail("Resource file not found: test stopped");
+        }
+
+        Path p = FileSystems.getDefault().getPath(resource.getFile());
+
+        String expected = "F8A80E86B361E0191FDE5250A321CCC3";
+        String actual = "";
+        try {
+            actual = MD5GenUtils.getHashOfFile(p).getHashHexString(true);
+        } catch (IOException e) {
+            fail(e.getMessage());
+        }
+
+        assertEquals(expected, actual);
+    }
 }
